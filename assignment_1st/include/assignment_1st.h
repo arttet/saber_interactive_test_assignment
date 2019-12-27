453991ac8eb861c24dac185a88cc496bdd52b3de
Artyom Tetyukhin <artemtetiukhin@yandex.com>
2019-12-27 20:30:56 +0300

    Add 1st assignment

--- assignment_1st/include/assignment_1st.h
+++ assignment_1st/include/assignment_1st.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <ostream>
+#include <iostream>
+#include <type_traits>
+
+namespace details
+{
+    template <typename T>
+    typename std::enable_if<std::is_integral<T>::value, std::ostream&>::type
+    out(std::ostream& out, T const value)
+    {
+        int8_t n_bits = sizeof(T) * CHAR_BIT - 1;
+        for (; n_bits >= 0; --n_bits)
+            out << ((value >> n_bits) & 1);
+
+        return out;
+    }
+}
+
+template <typename T>
+void print_bin_number(T value)
+{
+    details::out(std::cout, value);
+}
--- tests/test_assignment_1st/main.cpp
+++ tests/test_assignment_1st/main.cpp
@@ -2,7 +2,53 @@
 
 #include <assignment_1st.h>
 
+#include <bitset>
+#include <cstdint>
+#include <sstream>
+
+struct test_int32_t : testing::TestWithParam<int32_t>
+{
+};
+
+struct test_int8_t : testing::TestWithParam<int8_t>
+{
+};
+
+template <typename T>
+void test(T const value)
+{
+    std::ostringstream actual;
+    details::out(actual, value);
+
+    std::ostringstream expect;
+    expect << std::bitset<CHAR_BIT * sizeof(T)>(value);
+
+    EXPECT_EQ(actual.str(), expect.str());
+}
+
+TEST_P(test_int32_t, bitset)
+{
+    int32_t const value = GetParam();
+    test(value);
+}
+
+TEST_P(test_int8_t, bitset)
+{
+    int8_t const value = GetParam();
+    test(value);
+}
+
 // clang-format off
+
+INSTANTIATE_TEST_CASE_P(test_range, test_int32_t, testing::Range(-100, 100, 1));
+INSTANTIATE_TEST_CASE_P(test_limit_values, test_int32_t, testing::Values(
+    std::numeric_limits<int32_t>::min(),
+    std::numeric_limits<int32_t>::max()
+));
+
+INSTANTIATE_TEST_CASE_P(test_range, test_int8_t, testing::Range(
+    std::numeric_limits<int8_t>::min(), std::numeric_limits<int8_t>::max(), 1));
+
 // clang-format on
 
 int main(int argc, char ** argv)
