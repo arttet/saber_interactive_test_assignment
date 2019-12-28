5772e8fe60795559f6df8829ac2541221deba605
Artyom Tetyukhin <artemtetiukhin@yandex.com>
2019-12-28 15:06:51 +0300

    Code formated

--- 1st_assignment/include/1st_assignment.h
+++ 1st_assignment/include/1st_assignment.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include <ostream>
 #include <iostream>
+#include <ostream>
 #include <type_traits>
 
 namespace details
 {
     template <typename T>
-    typename std::enable_if<std::is_integral<T>::value, std::ostream&>::type
-    out(std::ostream& out, T const value)
+    typename std::enable_if<std::is_integral<T>::value, std::ostream &>::type out(std::ostream & out, T const value)
     {
         int8_t n_bits = sizeof(T) * CHAR_BIT - 1;
         for (; n_bits >= 0; --n_bits)
