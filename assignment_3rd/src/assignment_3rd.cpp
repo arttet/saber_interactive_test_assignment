8d6e4066a5713080ce8b02495f447f0cbce3dfbc
Artyom Tetyukhin <artemtetiukhin@yandex.com>
2019-12-28 14:16:04 +0300

    Add 3rd assignment

--- assignment_3rd/src/assignment_3rd.cpp
+++ assignment_3rd/src/assignment_3rd.cpp
@@ -1,11 +1,44 @@
 #include <assignment_3rd.h>
 
 #include <glm/glm.hpp>
+#include <glm/gtx/vector_angle.hpp>
 
-void calc_mesh_normals(glm::vec3 * /*normals*/,     //
-                       glm::vec3 const * /*verts*/, //
-                       int const * /*faces*/,       //
-                       int /*nverts*/,              //
-                       int /*nfaces*/)
+#include <vector>
+
+// https://stackoverflow.com/questions/45477806/general-method-for-calculating-smooth-vertex-normals-with-100-smoothness
+void calc_mesh_normals(glm::vec3 * normals,           //
+                       glm::vec3 const * const verts, //
+                       int const * const faces,       //
+                       int const nverts,              //
+                       int const nfaces)
 {
+    std::vector<std::vector<glm::vec3>> weighted_normals(nverts);
+
+    for (int i = 0; i < nfaces; i += 3) {
+        glm::vec3 const p1 = verts[faces[i]];
+        glm::vec3 const p2 = verts[faces[i + 1]];
+        glm::vec3 const p3 = verts[faces[i + 2]];
+
+        glm::vec3 n = glm::cross(p2 - p1, p3 - p1);
+        n = glm::normalize(n);
+
+        float const angle1 = glm::angle(p2 - p1, p3 - p1);
+        float const angle2 = glm::angle(p3 - p2, p1 - p2);
+        float const angle3 = glm::angle(p1 - p3, p2 - p3);
+
+        weighted_normals[faces[i]].push_back(n * angle1);
+        weighted_normals[faces[i + 1]].push_back(n * angle2);
+        weighted_normals[faces[i + 2]].push_back(n * angle3);
+    }
+
+    for (int i = 0; i < nverts; ++i) {
+        auto const & weighted_normal = weighted_normals[i];
+        glm::vec3 target_normal;
+
+        for (glm::vec3 const & n : weighted_normal) {
+            target_normal += n;
+        }
+
+        normals[i] = glm::normalize(target_normal);
+    }
 }
--- tests/test_assignment_3rd/main.cpp
+++ tests/test_assignment_3rd/main.cpp
@@ -2,7 +2,80 @@
 
 #include <assignment_3rd.h>
 
+#include <glm/glm.hpp>
+
+namespace
+{
+    bool load_obj(char const * const path,           //
+                  std::vector<glm::vec3> & vertices, //
+                  std::vector<int> & faces)
+    {
+        FILE * file = std::fopen(path, "r");
+        if (!file) {
+            return false;
+        }
+
+        char line_header[128] = { '\0' };
+
+        while (true) {
+
+            int const res = std::fscanf(file, "%s", line_header);
+            if (res == EOF)
+                break;
+
+            if (strcmp(line_header, "v") == 0) {
+                glm::vec3 vertex;
+                std::fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+                vertices.push_back(vertex);
+            } else if (strcmp(line_header, "f") == 0) {
+                int vertex_index[3];
+                int uv_index[3];
+                int normal_index[3];
+                int const matches = std::fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",             //
+                                                &vertex_index[0], &uv_index[0], &normal_index[0], //
+                                                &vertex_index[1], &uv_index[1], &normal_index[1], //
+                                                &vertex_index[2], &uv_index[2], &normal_index[2]);
+                if (matches != 9) {
+                    return false;
+                }
+
+                faces.push_back(vertex_index[0] - 1);
+                faces.push_back(vertex_index[1] - 1);
+                faces.push_back(vertex_index[2] - 1);
+            }
+        }
+
+        return true;
+    }
+}
+
+struct test_normals_t : testing::TestWithParam<char const *>
+{
+};
+
+TEST_P(test_normals_t, obj_file)
+{
+    char const * const path = GetParam();
+    std::vector<glm::vec3> vertices;
+    std::vector<int> faces;
+
+    bool const ok = load_obj(path, vertices, faces);
+    EXPECT_TRUE(ok);
+
+    std::vector<glm::vec3> actual_normals(vertices.size());
+    calc_mesh_normals(actual_normals.data(), //
+                      vertices.data(),       //
+                      faces.data(),          //
+                      vertices.size(),       //
+                      faces.size());         //
+}
+
 // clang-format off
+
+INSTANTIATE_TEST_CASE_P(test, test_normals_t, testing::Values(
+    "cube.obj"
+));
+
 // clang-format on
 
 int main(int argc, char ** argv)
